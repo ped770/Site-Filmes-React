8c581a25e06bdaee5c9d1944b778c3abaadd2468
Pedro Silva <pl4449803@gmail.com>
2023-03-03 14:26:46 -0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ function RoutesApp(){
             <Header/>
             <Routes>
                 <Route path='/' element = { <Home/> }/>
-                <Route path='/docs' element = { <Home/> }/>
+                <Route path='/Site-Filmes-React' element = { <Home/> }/>
                 <Route path='/filme/:id' element = { <Filme/> }/>
                 <Route path='/favoritos' element = { <Favoritos/> }/>
 
